a4fb7b81639b5648659e751c2232fa6f4a56a64c
arste <artem2006stepanov@yandex.ru>
2024-10-03 21:00:31 +0300

    create view shop + html sample

--- wordsnail/views.py
+++ wordsnail/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 __all__ = (
     "home",
+    "shop"
 )
 
 
@@ -10,3 +11,5 @@ def home(request):
     return render(request, "wordsnail/home.html")
 
 
+def shop(request):
+    return render(request, "wordsnail/shop.html")
