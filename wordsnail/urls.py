0b5ec1164bca65b0df98f9929f30636898eec22c
artem <artemyudin.2006@yandex.ru>
2024-10-07 16:39:43 +0300

    views login+register, html sample

--- wordsnail/urls.py
+++ wordsnail/urls.py
@@ -4,4 +4,6 @@ from .views import *
 
 urlpatterns = [
     path("", home, name="index"),
+    path("register/", register, name="register"),
+    path("login/", login, name='login')
 ]
--- wordsnail/views.py
+++ wordsnail/views.py
@@ -3,10 +3,22 @@ from django.shortcuts import render
 
 __all__ = (
     "home",
+    "register",
+    "login"
 )
 
 
 def home(request):
     return render(request, "wordsnail/home.html")
+
+
+def register(request):
+    return render(request, "wordsnail/register.html")
+
+
+def login(request):
+    return render(request, "wordsnail/login.html")
+
+
 
 
