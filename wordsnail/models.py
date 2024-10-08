82b0fc686d5676e5f2258c7e6539e2922b70c2bd
artem <artemyudin.2006@yandex.ru>
2024-10-09 00:12:47 +0300

    моделька юзера(риназ поменяй потом в мастере)

--- wordsnail/models.py
+++ wordsnail/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    current_skin = models.ImageField(upload_to="shopThings/gifs/", default="shopThings/gifs/6152297562.jpg")
+    money = models.IntegerField(default=0)
+    arr_skins = models.ManyToManyField(Shop)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'Users'
+        verbose_name_plural = 'Users'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
