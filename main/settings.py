939f84214102e980f65958cc2968b4978b9de5b3
rinaz <garaevribaz@gmail.com>
2024-10-20 14:30:12 +0300

    add staticfiles parametr

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$$$=5p3(-3%9yd-x--7=*w*-2$2e7gbjsj58hg8_vr@ds&u6*)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -117,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Папки, где Django будет искать статические файлы
 STATICFILES_DIRS = [
